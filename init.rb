55a7bd85882eb28f42e737f215924fe4ebecc19b
Derek Kastner <dkastner@gmail.com>
2010-09-15 20:05:15 -0400

    init.rb follows latest integrity api

--- init.rb
+++ init.rb
@@ -8,13 +8,13 @@ require "integrity"
 require "integrity/notifier/campfire"
 
 Integrity.configure do |c|
-  c.database  ENV["DATABASE_URL"]
-  c.directory "tmp"
-  c.base_url  ENV["URL"]
-  c.log       "tmp/integrity.log"
-  c.github    ENV["GITHUB_TOKEN"] || "TOKEN"
-  c.build_all = true
-  c.builder      :threaded, 5
-  c.user         ENV['HTTP_AUTH_USERNAME']
-  c.pass         ENV['HTTP_AUTH_PASSWORD']
+  c.database     = ENV["DATABASE_URL"]
+  c.directory    = "tmp"
+  c.base_url     = ENV["URL"]
+  c.log          = "tmp/integrity.log"
+  c.github_token = ENV["GITHUB_TOKEN"] || "TOKEN"
+  c.build_all    = true
+  c.builder      = :threaded, 5
+  c.username     = ENV['HTTP_AUTH_USERNAME']
+  c.password     = ENV['HTTP_AUTH_PASSWORD']
 end

7bfecd8ea5016e7d65286f2d949a76c4b2fcd72d
Derek Kastner <dkastner@gmail.com>
2011-03-07 20:13:27 -0500

    Prepend environment vars to project build command

--- lib/integrity/project.rb
+++ lib/integrity/project.rb
@@ -27,6 +27,14 @@ module Integrity
       builds.destroy!
     end
 
+    def command
+      if html?
+        "CUCUMBER_FORMAT=html RSPEC_FORMAT=html #{@command}"
+      else
+        @command
+      end
+    end
+
     def repo
       @repo ||= Repository.new(uri, branch)
     end
